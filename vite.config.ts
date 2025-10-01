e16432fa38e1d7095ae0c28f88b677b0e814bb4a
Tamara Stoof <tamara.stoof@emea.leaseplancorp.net>
2025-10-01 14:26:52 +0200

    tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue' // or react, etc.
+import { configDefaults } from 'vitest/config'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom', // or 'node' if you're not testing DOM
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+  },
 })
